2963502f2789220dcd29d95bbbd95c7627ec669b
SebastianG <dicoud@gmail.com>
2022-10-24 10:41:59 +0200

    Bouble Sort exercise

---
+++ boubleSort/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace boubleSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] number = { 300, 1, 60, 94, 61219, 41, 16, 7 };
+
+            int temporaryNumber = 0;
+
+            for (int i = 0; i < number.Length; i++)
+            {
+                for (int j = 0; j < number.Length - 1; j++)
+                {
+                    if (number[j] > number[j + 1])
+                    {
+                        temporaryNumber = number[j + 1];
+                        number[j + 1] = number[j];
+                        number[j] = temporaryNumber;
+                    }
+                }
+            }
+            Console.WriteLine("Lista po posortowaniu:");
+            for (int i = 0; i < number.Length; i++)
+            {
+                Console.Write($"{number[i]} {" "}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
